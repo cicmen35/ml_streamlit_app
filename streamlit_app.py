5deba4d82c13a9dd9e1be875a40ab5bce49fc9cd
Šimon Nemčič <138312285+cicmen35@users.noreply.github.com>
2025-03-08 23:08:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ with st.expander('Data'):
   df
 
 st.write('**X**')
-X = df.drop('species', axis=1)
-X
+X_raw = df.drop('species', axis=1)
+X_raw
 
 st.write('**y**')
-y = df.species
-y
+y_raw = df.species
+y_raw
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
@@ -29,7 +29,7 @@ with st.sidebar:
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male', 'female'))
+  sex = st.selectbox('Gender', ('male', 'female'))
 
   #DataFrame for the input features
   data = {'island': island,
@@ -37,13 +37,27 @@ with st.sidebar:
           'bill_depth_mm': bill_depth_mm,
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
-          'gender': gender}
+          'sex': sex}
   
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
-  
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+#Encode x
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
+
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[value]
+
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_df
-  st.write('**Combined penguines data**')
+  st.write('**Combined penguins data**')
   input_penguins
+  st.write('**Encoded input penguin**')
+  input_row
